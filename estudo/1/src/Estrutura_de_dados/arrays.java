59088ab6fa3b24a7e674dd2148781275abd61f56
Icaro-Cavalcante <icarodev06@gmail.com>
2026-02-17 11:34:26 -0300

    inicialização de arrays e memória

--- estudo/1/src/Estrutura_de_dados/arrays.java
+++ estudo/1/src/Estrutura_de_dados/arrays.java
@@ -16,5 +16,12 @@ public class arrays {
         for (int i = 0; i < tamanho; i++) {
             System.out.println(array[i]);
         }
+        String[] sarray = new String[1];
+        int[] iarray = new int[1];
+        boolean[] barray = new boolean[1];
+
+        System.out.println(sarray[0]);
+        System.out.println(iarray[0]);
+        System.out.println(barray[0]);
     }
 }
